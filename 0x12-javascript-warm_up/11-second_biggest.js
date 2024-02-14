02fea0c1b254122e47ca8ea6e4f999fb1eed0737
luka003-hub <kimathiluka03@gmail.com>
2024-02-14 13:46:34 -0800

    0x12-javascript-warm_up

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
 const args = process.argv.slice(2).map(Number);
+const sortedArgs = args.sort((a, b) => b - a);
 
 if (args.length <= 1) {
-  console.log(0);
+    console.log(0);
 } else {
-  const sortedArgs = args.sort((a, b) => b - a);
-  console.log(sortedArgs[1]);
+    console.log(sortedArgs[1]);
 }
